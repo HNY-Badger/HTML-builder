f978afae50d20d50091265e4036d3a97a3a98437
HNY_Badger <markelov-02@list.ru>
2024-01-16 20:32:44 +0400

    feat: 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,24 @@
+const fs = require('node:fs');
+const path = require('node:path');
+
+function errorNotifier(err) {
+  if (err) console.log(err);
+}
+
+async function bundleStyle(from, to) {
+  const stream = fs.createWriteStream(`${to}/bundle.css`);
+  await fs.readdir(from, (err, files) => {
+    errorNotifier(err);
+    files.forEach((file) => {
+      if (path.extname(file) === '.css') {
+        fs.readFile(`${from}/${file}`, (err, data) => {
+          errorNotifier(err);
+          console.log(`Bundling ${file}...`);
+          stream.write(`/* Styles from the ${file} */\n${data}\n`);
+        });
+      }
+    });
+  });
+}
+
+bundleStyle('./05-merge-styles/styles', './05-merge-styles/project-dist');
