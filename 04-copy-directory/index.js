36f0f85cd849a7658d8f7cc955ca3a54d409e4dd
HNY_Badger <markelov-02@list.ru>
2024-01-20 12:00:21 +0400

    fix: 04-copy-directory deletes files aswell

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,21 +1,24 @@
 const fs = require('node:fs');
 
-function errorNotifier(err) {
+function errNotify(err) {
   if (err) console.log(err);
 }
 
 function copyDir(from, to) {
-  fs.mkdir(to, errorNotifier);
-  fs.readdir(from, (err, files) => {
-    errorNotifier(err);
-    files.forEach((file) => {
-      fs.stat(`${from}/${file}`, (err, stats) => {
-        errorNotifier(err);
-        if (stats.isFile()) {
-          fs.copyFile(`${from}/${file}`, `${to}/${file}`, errorNotifier);
-        } else if (stats.isDirectory()) {
-          copyDir(`${from}/${file}`, `${to}/${file}`);
-        }
+  fs.rm(to, { recursive: true, force: true }, (err) => {
+    errNotify(err);
+    fs.mkdir(to, errNotify);
+    fs.readdir(from, (err, files) => {
+      errNotify(err);
+      files.forEach((file) => {
+        fs.stat(`${from}/${file}`, (err, stats) => {
+          errNotify(err);
+          if (stats.isFile()) {
+            fs.copyFile(`${from}/${file}`, `${to}/${file}`, errNotify);
+          } else if (stats.isDirectory()) {
+            copyDir(`${from}/${file}`, `${to}/${file}`);
+          }
+        });
       });
     });
   });
